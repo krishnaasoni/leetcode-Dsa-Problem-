a5b54b081863dfb7e9c9025ed298b2750359ef15
Krishna Soni <krishnaasoni2006@gmail.com>
2026-07-27 20:14:03 +0530

    Time: 19 ms (12.61%), Space: 52.4 MB (90.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,45 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//     HashMap<Integer, Integer> map= new HashMap<>();
+//      int n= nums.length;
+
+//     for (int i=0; i<n; i++){
+//           if(map.containsKey(nums[i])){
+//             map.put(nums[i], map.get(nums[i])+1);
+//         }
+//         else{
+//             map.put(nums[i], 1);
+//         }
+//     }
+//            for (int key : map.keySet()){
+//                     if(map.get(key)> n/2){
+//                         return key;
+//                     }
+//            }
+//            return 0;
+//     }
+// }
+
+
+
 class Solution {
     public int majorityElement(int[] nums) {
     HashMap<Integer, Integer> map= new HashMap<>();
      int n= nums.length;
 
-    for (int i=0; i<n; i++){
-          if(map.containsKey(nums[i])){
-            map.put(nums[i], map.get(nums[i])+1);
+    for (int i: nums){
+        if(map.containsKey(i)){
+            int val= map.get(i);
+            map.put(i, val+1);
         }
         else{
-            map.put(nums[i], 1);
+            map.put(i, 1);
+        }
+        if(map.get(i)> n/2){
+            return i;
         }
     }
-           for (int key : map.keySet()){
-                    if(map.get(key)> n/2){
-                        return key;
-                    }
-           }
+
            return 0;
     }
 }
