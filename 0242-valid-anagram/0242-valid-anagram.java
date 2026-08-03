d8fda2e5a07ff87dc07602f33e0a5967d9e88db4
Krishna Soni <krishnaasoni2006@gmail.com>
2026-08-03 19:24:21 +0530

    Time: 4 ms (82.71%), Space: 46.8 MB (9.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         HashMap<Integer, Integer> map= new HashMap<>();
+        //  HashMap<Integer, Integer> map= new HashMap<>();
          char[] arr1 = s.toCharArray();
          char[] arr2 = t.toCharArray();
 
@@ -8,10 +8,7 @@ class Solution {
           Arrays.sort(arr1);
           Arrays.sort(arr2);
 
-          if(Arrays.equals(arr1, arr2)){
-            return true;
-        
-          }
-            return false;    
+        return    Arrays.equals(arr1, arr2);
+               
     }
 }
